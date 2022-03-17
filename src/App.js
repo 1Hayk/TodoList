7da22d35ed1168a5c81e31ca8b586c9d83524534
1Hayk <haykmk97@gmail.com>
2022-03-17 18:07:40 +0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import {useState} from 'react'
-import './App.css';
-import { TodoListHeader } from './Components/TodoListHeader';
+import { useState } from "react";
 
-
-
-
-
-
-function App() {
  
-  
-  
-  return (
-    <div>
-     <TodoListHeader />
-     
-    </div>
-    
-    
-  )
-}
+ export const App = () => {
 
-export default App;
+     const initialState = {
+         firstNumber:'',
+         symbol:'',
+         secondNumber:'',
+         result:0
+     }
+
+     const [state, setState] = useState(initialState)
+
+     const {firstNumber, symbol, secondNumber, result} = state
+     const numbers =  ['.',9,8,7,6,5,4,3,2,1,0,]
+     const symbols = ['+', '-', '*', '/', '=']
+
+
+
+     const func = symb => {
+         if(firstNumber){
+
+             setState({...state, symbol:symb})
+             
+             calculate(symb)
+         }
+       
+        
+        
+     }
+
+
+     
+     const calculate = (symb) => {
+         if(symbol === '+'){
+             setState({...state, symbol:symb, result:firstNumber + secondNumber,  firstNumber:firstNumber + secondNumber, secondNumber:''})
+         }else if(symbol === '-'){
+            setState({...state, symbol:symb,  result:firstNumber - secondNumber, firstNumber:firstNumber - secondNumber, secondNumber:''})
+         }else if(symbol === '*'){
+            setState({...state, symbol:symb, result:firstNumber * secondNumber, firstNumber:firstNumber * secondNumber, secondNumber:''})
+         }else if(symbol === '/'){
+            setState({...state, symbol:symb, result:firstNumber / secondNumber, firstNumber:firstNumber / secondNumber, secondNumber:''})
+         }
+     }
+
+
+console.log(state);
+     const plusNumber = (value) => {
+         if(symbol === ''){
+             setState({...state, firstNumber:+(firstNumber + value)})
+         }else{
+             setState({...state,   secondNumber: +(secondNumber + value)})
+         }
+         
+         
+     }
+
+   
+    
+  return(
+      
+
+    <div>
+        <h1>{firstNumber}{symbol}{secondNumber}</h1>
+        <h2>{result}</h2>
+            {
+                numbers.map(number => <button value={number} onClick={(e) => plusNumber(e.target.value)} key={number}>{number}</button>)
+            }
+            {
+                symbols.map(item => <button value={item} onClick = {(e) => func(e.target.value)} key={item}>{item}</button>)
+            }
+            <button onClick={() => setState({...initialState, symbol: '', firstNumber:'', secondNumber:'', result:0})}>AC</button>
+            
+            
+    </div>
+     
+   
+      
+  )
+  }
+
+export default App
